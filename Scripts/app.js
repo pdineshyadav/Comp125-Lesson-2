c1c591f355be04e258aafb6f85c24b350f926939
pdineshyadav <p.dineshyadav@yahoo.in>
2016-05-16 09:37:58 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,17 @@
+// Main Javascript file
+
+//IIFE - Immediate Invoked Function Expression
+
+(function(){
+    "use strict";
+    // code starts in here
+    Console.log("App Started....");
+    
+    
+    
+    
+    
+})();
+
+
+
