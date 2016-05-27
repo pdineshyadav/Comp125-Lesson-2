6be50790b46e5eaf70374ed7f7a6911e7c8984fc
pdineshyadav <p.dineshyadav@yahoo.in>
2016-05-27 00:22:52 -0400

    Updated app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,5 @@
 /* main JavaScript file */
 
-// Global Scope
 
 
 // IIFE - Immediately Invoked Function Expression
@@ -8,14 +7,42 @@
     "use strict";
    // Code goes in here
    console.log("App Started...")
-    // Create a reference to a p element of the id of "firstParagraph"
-    var firstParagraph = document.getElementById("firstParagraph");
     
-    firstParagraph.textContent = "It's alive";
+    /*
+    // Declared Named Function
+    function writeToFirstParagraph()
+    {
+        // For debugging only
+        console.log("Executing writeToFirstParagraph");
+        
+        // Create a reference to a p element of the id of "firstParagraph"
+    var firstPTag = document.getElementById("firstParagraph");
     
-    //
+    firstPTag.textContent = "It's alive";
+ 
     
+    }
+    */
     
+    //Declare a variable that points to annoymous function
+    var writeToFirstParagraph = function()
+    {
+        // For debugging only
+        console.log("Executing writeToFirstParagraph");
+        
+        // Create a reference to a p element of the id of "firstParagraph"
+        var firstPTag = document.getElementById("firstParagraph");
+        
+        firstPTag.textContent = "It's alive";
+
+    }
+
+
+    
+    // Execute function
+    writeToFirstParagraph();
+    
+    console.log(firstParagraph);
 })();
 
 
