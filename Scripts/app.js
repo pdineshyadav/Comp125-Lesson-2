656ff99ce6333c59dc01488b33fbeee8b03b7541
pdineshyadav <p.dineshyadav@yahoo.in>
2016-05-26 20:40:25 -0400

    Adder simple code to app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,15 +1,17 @@
 /* main JavaScript file */
 
+// Global Scope
+
+
 // IIFE - Immediately Invoked Function Expression
 (function(){
     "use strict";
+   // Code goes in here
+   console.log("App Started...")
+    // Create a reference to a p element of the id of "firstParagraph"
+    var firstParagraph = document.getElementById("firstParagraph");
     
-    
-    // code codes in here 
-    console.log("App Started...");
-    
-    
-    
+    firstParagraph.textContent = "It's alive";
     
     
     
